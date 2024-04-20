13b6eb20e141635bdc3a907c88752878294953e2
miravuong <158006280+miravuong@users.noreply.github.com>
2024-04-20 05:23:40 -0700

    kill me 1
    5:23 AM

---
+++ uquest/src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Navbar() {
+
+    return (
+        <nav className="nav">
+            <a href="/leaderboard">
+                <img class="logo" src="/images/Logo.png" alt="logo" />
+                <p>U-Quest</p>
+            </a>
+            <a href="/leaderboard">Leaderboard</a>
+            <a href="/house">House</a>
+            <a href="/league">League</a>
+        </nav>
+
+    )
+
+
+}
