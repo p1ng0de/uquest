bed6e2f46a9913109735db72acb3eeff5632d601
miravuong <158006280+miravuong@users.noreply.github.com>
2024-04-20 15:41:07 -0700

    Worked on league

--- uquest/src/components/League.js
+++ uquest/src/components/League.js
@@ -2,24 +2,27 @@
 export default function League() {
     return (
         <div>
-            <div id="line5"></div>
-            <p id="lname">Insert League Name</p>
+            <div id="line"></div>
             <div id="line4"></div>
+            <p id="lname">Insert League Name</p>
+            <div id="line5"></div>
 
             <div id="overall-container">
-                <div id="container-a">
-                    <div className="card1">
+                <div className="container-left">
+                    <div className="c1">
                         Top 3
                     </div>
-                    <div className="card2">
+                    <div className="c2">
                         League info
                     </div>
                 </div>
-            </div>
-
-            <p>Submission feed</p>
-            <div id="container-b">
-                Hello
+                <div className="container-b">
+                    <p>Submission feed</p>
+                    <div className="bluebox"></div>
+                </div>
+                <div className="container-c">
+                    Tasks
+                </div>
             </div>
         </div>
 
